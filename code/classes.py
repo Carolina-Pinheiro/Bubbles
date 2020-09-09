36a98faa20354efc65990ceb63897ce0d2b5e7ba
cpp2599ist <57190987+cpp2599ist@users.noreply.github.com>
2020-09-09 04:01:35 +0100

    classes

--- code/classes.py
+++ code/classes.py
@@ -1,9 +1,10 @@
 #Config Class
 class config:
-    def __init__(self,height, width, r, dl, initial_lines, N_moves ):
+    def __init__(self,height, width, r, dl, initial_lines, N_moves, screen ):
         self.height= height
         self.width= width
         self.r= r
         self.dl= dl
         self.initial_lines= initial_lines
         self.N_moves= N_moves
+        self.screen= screen
