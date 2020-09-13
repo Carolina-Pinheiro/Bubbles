1e97140690038ef879af67e73bafb5349f1e9de2
cpp2599ist <57190987+cpp2599ist@users.noreply.github.com>
2020-09-13 04:06:37 +0100

    more organized

--- code/main.py
+++ code/main.py
@@ -15,6 +15,7 @@ import time
 
 #######
 #Main
+#Get game configuration
 config= pap.pre_init()
 
 #Check config
@@ -22,54 +23,24 @@ config= pap.pre_init()
 
 #Initialize game
 config=ggraph.init_window(config)
-bubbles=ingame.init_board(config)
-config.colors_list=ingame.load_colors(config)
 
 #Initialize Variables
-bubble_in_play=[classes.bubble(config.r, config.height+ggraph.SIZE_BOARD-config.r, random.randrange(1,10) ),
-                classes.bubble_moving(int(config.width/2), config.height+ggraph.SIZE_BOARD-config.r, random.randrange(1,10), 0, False)]
-
-background = pygame.image.load('./images/fundo.jpg')
-background2 = pygame.image.load('./images/fundo2.jpg')
-
-background_sized= [pygame.transform.scale(background, (config.width, config.height))]
-background_sized.append(pygame.transform.scale(background2, (config.width, ggraph.SIZE_BOARD)))
+bubbles, bubble_in_play, background_sized= ingame.initialize_variables(config)
 game= True
 
 #Game Loop
 while game:
-    game, bubbles=ingame.game_loop(background_sized, config,bubbles,bubble_in_play, game)
+    game, bubbles = ingame.game_loop(background_sized, config,bubbles,bubble_in_play, game)
+
+#Short Pause so the player can see the board before the game over screen
+time.sleep(0.75) 
 
 #Game Over Screen
 game_over=True
-score=0
-time.sleep(0.75)
+background_sized= pygame.transform.scale(pygame.image.load('./images/fundo.jpg'), (config.width, config.height+ ggraph.SIZE_BOARD))
 name=''
-background_sized= pygame.transform.scale(background, (config.width, config.height+ ggraph.SIZE_BOARD))
-
 while game_over:
-    config.screen.blit(background_sized, (0, 0))
-    ggraph.game_over_screen(config, score)
-    myfont = pygame.font.SysFont('lucidaconsole', int(ggraph.SIZE_BOARD))
-    get_input=True
-
-    for event in pygame.event.get():
-        #Close Window
-        if event.type == pygame.QUIT:
-            game_over= False
-        #Get input text
-        if event.type == pygame.KEYDOWN:
-            name, game_over=pap.text_input(event,name)
-    
-    #Text Box
-    write_rect=pygame.Rect (int(0.09*config.width), int(0.69*config.height), int(0.85*config.width), int(1.1*ggraph.SIZE_BOARD) )
-    pygame.draw.rect(config.screen, (255,255,255), write_rect)
-    pygame.draw.rect(config.screen, (0,0,0), write_rect, 2)
-
-    #Write Input
-    surface= myfont.render(str(name), False, (0,0,0))
-    config.screen.blit(surface,(int(0.10*config.width), int(0.7*config.height)) )
-    pygame.display.update()
+    game_over, name = ingame.game_over_loop(config, background_sized, game_over, name)
 
 #Write in the results file
 pap.write_results(name, config.score)
